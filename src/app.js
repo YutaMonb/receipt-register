a37c89d2c5373c186e9c578c763b6197a6e463cd
yutamonb <yutamonb@gmail.com>
2019-02-03 12:03:24 +0900

    opnerで開けるように

--- src/app.js
+++ src/app.js
@@ -1,26 +1,35 @@
 "use stirct";
 
 const fs = require("fs");
+const opener = require("opener");
 
 /**
- * 
- * @param {string} path 
- * @param {Array} callback 
+ *
+ * @param {string} path
  */
-const getFiles = (path, callback) => {
-  const imageList = [];
-  fs.readdir(path, { withFileTypes: true }, (err, dirents) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+const getDirFiles = async path =>
+  new Promise(resolve => {
+    const imageList = [];
+    fs.readdir(path, { withFileTypes: true }, (err, dirents) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
 
-    dirents.forEach(dirent => {
-      if (!dirent.name.match(/(\.jpeg|\.jpg|\.png)/i)) return;
-      imageList.push(dirent.name);
+      dirents.forEach(dirent => {
+        if (!dirent.name.match(/(\.jpeg|\.jpg|\.png)/i)) return;
+        imageList.push(dirent.name);
+      });
+      resolve(imageList);
     });
-    callback(imageList);
   });
-};
 
-getFiles(process.argv[2], console.log);
+async function main() {
+  const files = await getDirFiles(process.argv[2]);
+
+  files.forEach(file => {
+    opener(process.argv[2] + "/" + file);
+  });
+}
+
+main();
