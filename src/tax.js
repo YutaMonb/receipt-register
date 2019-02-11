9957eaf7b6f3e5e8af1384c15e68d237e24cdba2
yutamonb <yutamonb@gmail.com>
2019-02-11 16:49:47 +0900

    total表示

--- src/tax.js
+++ src/tax.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 
 fs.readFile('./data/output.json', (err, data) => {
   const json = JSON.parse(data);
-  
+  let total = 0;
   let map = new Map();
   json.forEach(element => {
     if(!map.has(element.type)) {
       map.set(element.type, 0);
     }
     map.set(element.type, map.get(element.type) + element.price);
+  total += element.price;
   });
   console.log(map);
+  console.log(total);
 });
